ce4d0637ab0d069bc8296133163311a5c02512f4
SunCreamGames <yasinchakb@gmail.com>
2022-06-07 22:41:05 +0300

    commint

---
+++ Api/LabThree/LabThree.Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LabThree.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabThree.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AccordionEntity> Accordions { get; set; }
+}
